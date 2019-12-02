50cf301d1eb6b3edc7815d08a26d8828db483a7c
Blake Olsen <bolsen@hopper.com>
2019-12-02 17:10:09 -0600

    part one

---
+++ day1/part1.py
@@ -0,0 +1,14 @@
+import csv
+
+def extrac_csv():
+    csv.reader()
+
+def main():
+    with open('input.csv') as input:
+        masses = csv.reader(input)
+        fuels = [int(int(mass[0])/3)-2 for mass in masses]
+        return sum(fuels)
+
+if __name__ == "__main__":
+    print("Advent of Code 2019 - Day 1")
+    print(main())
